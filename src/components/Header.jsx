8e4a56deac7bbb38d6c587c928ccdba3dd5884a3
Michael <kllmmc23@gmail.com>
2020-06-05 19:53:29 -0600

    sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return <header className="header">HEADER</header>;
-}
+  return (
+    <header className="header">
+          <img src="img/logo.png" alt=" nexter logo" className="header__logo" />
+          <h3 className="heading-3">Your own home</h3>
+          <h1 className="heading-1">The ultimate personal freedom</h1>
+          <button className='btn header__btn'>view our properties</button>
+          <div className="header__seenon-text">Seen on</div>
+          <div className="header__seenon-logos">
+              <img src="img/logo-bbc.png" alt="Seeon on logo 1"/>
+              <img src="img/logo-forbes.png" alt="Seeon on logo 2"/>
+              <img src="img/logo-techcrunch.png" alt="Seeon on logo 3"/>
+              <img src="img/logo-bi.png" alt="Seeon on logo 4"/>
+
+          </div>
+
+    </header>
+  );
+};
 
 export default Header;
